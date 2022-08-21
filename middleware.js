cfab19caf5061bd273ed122fa7d5217dc5b427b7
samnomadgate <81360115+samnomadgate@users.noreply.github.com>
2022-08-20 22:10:38 -0300

    more styles

--- middleware.js
+++ middleware.js
@@ -27,7 +27,7 @@ module.exports.isReviewAuthor = async (req, res, next) => {
     if (!review.author.equals(req.user._id)) {
         if(!req.user.isAdmin) {
         req.flash('error', 'You do not have permission to do that!');
-        return res.redirect(`/campgrounds/${id}`);
+        return res.redirect(`/coliving/${id}`);
         }
     }
     next();
